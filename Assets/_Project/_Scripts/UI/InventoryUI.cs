2cec9ff09d2da2c0ae60a086912741c5fe1c818f
obolulu <ozan.bolulu@yahoo.com>
2024-11-18 21:11:37 +0300

    moose

--- Assets/_Project/_Scripts/UI/InventoryUI.cs
+++ Assets/_Project/_Scripts/UI/InventoryUI.cs
@@ -39,7 +39,7 @@ public class InventoryUI : MonoBehaviour
         items.Clear();
         ClearInventoryDisplay();
         items = playerStatsHandler.GetInventoryItems();
-        DrawInventoryList();
+        //DrawInventoryList();
     }
 
     private void ClearInventoryDisplay()
